622c0af9a897564ffa2bf716a80e2bb6f93a26a3
Connor Jacobsen <jacobsen.connor@gmail.com>
2016-10-18 14:05:23 -0700

    update mix dependencies to support ecto 2.1-rc2

--- mix.exs
+++ mix.exs
@@ -36,10 +36,10 @@ defmodule ExMachina.Mixfile do
     [
       {:ex_doc, "~> 0.9", only: :dev},
       {:earmark, ">= 0.0.0", only: :dev},
-      {:ecto, "~> 2.0", optional: true},
-      {:ecto, "~> 2.0", only: [:dev, :test]},
+      {:ecto, "~> 2.1-rc2", optional: true},
+      {:ecto, "~> 2.1-rc2", only: [:dev, :test]},
       {:postgrex, ">= 0.0.0", only: [:test]},
-      {:poison, "~> 2.0.0", only: :test}
+      {:poison, "~> 2.2.0", only: :test}
     ]
   end
 
